450bf1c10751b08d990607205d4a2d574b4cb4bb
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-08 01:33:51 +0200

    [upd] shoppingList js code

--- lista-spesa.js
+++ lista-spesa.js
@@ -1,2 +1,21 @@
 'use strict'
 
+//prendiamo l'id ul dal dom
+const ul = document.getElementById('ul');
+
+//arraylist spesa
+const shoppingList = [
+    'pomodori',
+    'carote',
+    'sedano',
+    'lattuga',
+    'acqua',
+];
+
+let i = 0; // Inizializzazione della variabile
+while (i < shoppingList.length) { // Condizione di uscita
+    const listItem = document.createElement('li'); // Creazione di un nuovo elemento li
+    listItem.textContent = shoppingList[i]; // Impostazione del testo dell'elemento li con l'elemento corrente della lista della spesa
+    ul.append(listItem);
+    i++;
+}
